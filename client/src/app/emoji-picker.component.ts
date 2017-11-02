1a87e8836f8ef9b1176b14e74a746ecc3f796917
Noah Mercer <noah_ten@yahoo.com>
2017-11-02 14:14:27 -0700

    Simple emoji picker.

---
+++ client/src/app/emoji-picker.component.ts
@@ -0,0 +1,32 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+    selector: 'emoji-picker',
+    template: `
+    <div ngbDropdown class="d-inline-block">
+        <button class="btn" id="emoji-toggle" ngbDropdownToggle>{{toggleIcon}}</button>
+        <div ngbDropdownMenu class="emoji-menu" aria-labelledby="dropdownBasic1">
+            <button *ngFor="let emoji of availableEmojis" (click)="emojiPicked($event)" class="dropdown-item">{{emoji}}</button>
+        </div>
+    </div>
+    `,
+    styleUrls: ['../assets/css/emoji.css']
+})
+
+export class EmojiPickerComponent implements OnInit {
+    availableEmojis: string[];
+    toggleIcon: string;
+    @Output() onSelected = new EventEmitter<string>();
+
+    constructor() {
+        this.toggleIcon = '😀';
+        this.availableEmojis = ['😀', '😎', '💩', '🙀', '😰'];
+     }
+
+    emojiPicked(event: any) {
+        this.toggleIcon = event.target.innerText;
+        this.onSelected.emit(event.target.innerText);
+    }
+
+    ngOnInit() { }
+}
