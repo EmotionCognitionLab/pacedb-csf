57b1e59391e85bea3de4399f8f74c5dd00f8e6ce
Noah Mercer <noah_ten@yahoo.com>
2017-11-10 10:20:35 -0800

    Styling for group message display

--- client/src/app/group-page.component.ts
+++ client/src/app/group-page.component.ts
@@ -13,19 +13,21 @@ import { GroupService } from './service/group.service';
 @Component({
     selector: 'app-group-page',
     template: `
-    <div class="container">
+    <div class="container-narrow">
         <h2>Teammates</h2>
         <app-user *ngFor="let user of members" [user]=user [doneRatio]=85></app-user>
         <hr />
         <h2>Messages</h2>
         <div class="form-group">
             <form (ngSubmit)="sendGroupMsg()" #msgForm="ngForm">
-                <div style="float: left;">
-                    <textarea rows="2" cols="40" name="msgText" required #msg="ngModel" [(ngModel)]="msgText" type="textarea" class="group-msg-txt" placeholder="Send a message to the rest of the group"></textarea>
+                <div class="textarea-container">
+                    <div class="title">Send a message to the group</div>
+                    <textarea rows="3" cols="70" name="msgText" required #msg="ngModel" [(ngModel)]="msgText" type="textarea" class="group-msg-txt"></textarea>
                 </div>
                 <button class="btn msg-text-btn" [disabled]="!msgForm.form.valid" type="submit">Submit</button>
             </form>
         </div>
+        <div style="line-height: 0.5em;">&nbsp;</div>
         <group-message *ngFor="let msg of messages" [msg]=msg [nameFn]=nameFinder(this) [photoFn]=photoFinder(this)></group-message>
     </div>
     `
@@ -51,7 +53,7 @@ export class GroupPageComponent implements OnInit {
     sendGroupMsg() {
         const message = new GroupMessage(this.msgText.trim());
         this.groupService.createGroupMessage(message, this.name).subscribe(savedMsg => {
-            this.messages.push(savedMsg);
+            this.messages.unshift(savedMsg);
         });
         this.msgText = '';
     }
