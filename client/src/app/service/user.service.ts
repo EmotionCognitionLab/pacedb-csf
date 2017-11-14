cbae367203fe942cf4410e2f1c3dbdfe51bcfeab
Noah Mercer <noah_ten@yahoo.com>
2017-11-14 13:44:32 -0800

    Adds caching to UserService

--- client/src/app/service/user.service.ts
+++ client/src/app/service/user.service.ts
@@ -1,17 +1,30 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers, URLSearchParams } from '@angular/http';
-import { RequestMethod, RequestOptions, RequestOptionsArgs } from '@angular/http';
+import { RequestMethod, RequestOptions, RequestOptionsArgs, ResponseOptions } from '@angular/http';
 import { Response, ResponseContentType } from '@angular/http';
 
 import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
 
 import { AuthService } from '../service/auth.service';
 import { User } from '../model/user';
 import { environment } from '../../environments/environment';
 
+
+interface Cacheable {
+    item: any;
+    expiration: number;
+}
+
 @Injectable()
 export class UserService {
+    static DEFAULT_MAX_AGE = 30 * 60 * 1000; // 30 minutes (in ms)
+
     basePath = environment.apiBasePath;
+    private _userCache = new Map<string, Cacheable>();
+    private _inFlightRequests = new Map<string, Subject<any>>();
+    // JSON string for user object to return when a user can't be found
+    private _anonUser = `{"firstName": "Unknown", "lastName": "U", "isAdmin": false, "photoUrl": "https://${environment.usrImgBucket}.s3.${environment.awsRegion}.amazonaws.com/medium/missing-icon.jpg"}`;
 
     constructor(private authService: AuthService, private http: Http) { }
 
@@ -21,14 +34,37 @@ export class UserService {
      * @param userId id of the user to fetch
      */
     public getUser(userId: string, extraHttpRequestParams?: any): Observable<User> {
-        return this.getUserWithHttpInfo(userId, extraHttpRequestParams)
+        const result = this.cacheGet(userId);
+        if (result !== undefined) {
+            return result;
+        }
+        const userFetch = this.getUserWithHttpInfo(userId, extraHttpRequestParams)
+            .catch((err) => {
+                if (err instanceof Error || err['status'] === undefined || err['status'] !== 404) {
+                    throw err;
+                } else {
+                    // return an anonymous user response so we don't continually try to re-fetch
+                    const resp = new Response(new ResponseOptions({
+                        body: this._anonUser,
+                        headers: err.headers,
+                        url: err.url,
+                        status: 200
+                    }));
+                    return Observable.of(resp);
+                }
+            })
             .map((response: Response) => {
                 if (response.status === 204) {
                     return undefined;
                 } else {
-                    return response.json() || {};
+                    const obj = response.json() || {};
+                    const user = User.fromJsonObj(obj);
+                    this.cacheSet(userId, user);
+                    return user;
                 }
             });
+        this._inFlightRequests.set(userId, new Subject());
+        return userFetch;
     }
 
     /**
@@ -51,7 +87,6 @@ export class UserService {
 
         let requestOptions: RequestOptionsArgs = new RequestOptions({
             method: RequestMethod.Get,
-            headers: headers,
             search: queryParameters,
             withCredentials: false
         });
@@ -67,6 +102,37 @@ export class UserService {
             return this.http.request(path, requestOptions);
         });
 
+    }
+
+    private cacheGet(userId: string): Observable<User> | Subject<any> | undefined {
+        if (this._userCache.has(userId)) {
+            if (this._userCache.get(userId).expiration > Date.now()) {
+                return Observable.of(this._userCache.get(userId).item);
+            } else {
+                this._userCache.delete(userId);
+                return this._inFlightRequests.get(userId);
+            }
+        }
+        return this._inFlightRequests.get(userId);
+    }
+
+    /**
+     * Sets a user in the internal cache.
+     * @param {string} userId Id of the user to set
+     * @param {User} user The user to set
+     * @param {number} maxAge The number of milliseconds this cache entry should be considered valid
+     */
+    cacheSet(userId: string, user: User, maxAge: number = UserService.DEFAULT_MAX_AGE): void {
+        this._userCache.set(userId, {item: user, expiration: Date.now() + maxAge});
+        if (this._inFlightRequests.has(userId)) {
+            // notify any observers and remove it from the in-flight requests
+            const inFlight = this._inFlightRequests.get(userId);
+            if (inFlight.observers.length > 0) {
+                inFlight.next(user);
+            }
+            inFlight.complete();
+            this._inFlightRequests.delete(userId);
+        }
     }
 
 
