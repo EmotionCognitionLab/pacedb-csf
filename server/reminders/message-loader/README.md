824a890f2fe4dfb801ea6319d7cc53246bcdeb1f
Noah Mercer <noah_ten@yahoo.com>
2017-11-23 12:00:59 -0800

    Loader utility to put messages into hrv-reminders-msgs table

---
+++ server/reminders/message-loader/README.md
@@ -0,0 +1,10 @@
+This directory contains the raw data for the various reminder messages as well as a script to load them into the hrv-reminder-messages table. Note that if you want to change the data the following
+must be true:
+
+ - They must be in csv format
+ - The first line of the file must be the column names
+ - There must be a column named 'id' with numeric values (to match the dynamodb hash key)
+ - They must be in the UTF-8 character set
+
+To run it:
+`node load-msgs.js region csv-file`
---
+++ server/reminders/message-loader/load-msgs.js
@@ -0,0 +1,60 @@
+'use strict'
+
+const myArgs = process.argv.slice(2);
+if (myArgs.length !== 2) {
+    console.log('Usage: node load-msgs.js region csv-file');
+    return;
+}
+const region = myArgs[0];
+const csvFile = myArgs[1];
+
+const AWS = require('aws-sdk');
+const dynamo = new AWS.DynamoDB.DocumentClient({region: region, apiVersion: '2012-08-10'});
+
+const fs = require('fs');
+const parse = require('csv-parse');
+
+const msgData = fs.readFileSync(csvFile, 'utf8');
+parse(msgData, {trim: true, columns: true, auto_parse: true, skip_empty_lines: true, skip_lines_with_empty_values: true},
+function(err, csvRecs) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    const items = []
+
+    csvRecs.forEach(r => {
+        // the auto_parse option on csv-parse doesn't seem to handle booleans
+        if (r.active.toLowerCase() === 'true') {
+            r.active = true;
+        } else {
+            r.active = false;
+        }
+        items.push({PutRequest: {Item: r}})
+    });
+    const pushCmd = {};
+    let pushed = 0;
+    while (pushed < items.length) {
+        // dynamo batchWrite accepts a max of 25 items
+        let loaded = 0;
+        if (pushed+25 >= items.length) {
+            pushCmd['hrv-reminder-msgs'] = items.slice(pushed);
+            loaded = items.length - pushed;
+        } else {
+            pushCmd['hrv-reminder-msgs'] = items.slice(pushed, pushed+25);
+            loaded = 25;
+        }
+        console.log(`Loaded ${loaded} items`);
+
+        dynamo.batchWrite({RequestItems: pushCmd}).promise()
+        .then((result) => {
+           if (Object.keys(result.UnprocessedItems).length > 0) {
+               console.log('Failed to process the following items: ');
+               console.log(JSON.stringify(result.UnprocessedItems));
+           }
+        });
+        pushed += 25;
+        
+    }
+    
+});
