EmotionCognitionLab/pacedb-csf

commit f7858e6560ffb68400ab59a39facc18613b82164

Replaces s3Endpoint (which wasn't set in env vars) with region.

server/training-data-import/import.js
@@ -2,11 +2,10 @@
 
 const AWS = require('aws-sdk');
 const dynamoEndpoint = process.env.DYNAMO_ENDPOINT;
-const s3Endpoint = process.env.S3_ENDPOINT;
 const region = process.env.REGION;
 const bucket = process.env.DATA_BUCKET;
 const dynamo = new AWS.DynamoDB.DocumentClient({endpoint: dynamoEndpoint, apiVersion: '2012-08-10'});
-const s3 = new AWS.S3({endpoint: s3Endpoint, apiVersion: '2006-03-01', s3ForcePathStyle: true});
+const s3 = new AWS.S3({region: region, apiVersion: '2006-03-01', s3ForcePathStyle: true});
 
 const sqlite3 = require('better-sqlite3');
 const parse = require('csv-parse');

commit 34c2411ab97fc0cee0803cb567b142a866a61e7b

Restores s3 endpoint; commit f7858e6560 broke tests.

server/serverless.yml
@@ -243,7 +243,7 @@ functions:
         Fn::Sub: "${self:service}-${self:provider.stage}-usr-data"
       GROUPS_TABLE:
         Ref: groupsTable
-      REGION: us-east-2
+      S3_ENDPOINT: "https://s3.us-east-2.amazonaws.com"
 
 plugins:
   - serverless-dynamodb-local

server/training-data-import/import.js
@@ -2,10 +2,11 @@
 
 const AWS = require('aws-sdk');
 const dynamoEndpoint = process.env.DYNAMO_ENDPOINT;
+const s3Endpoint = process.env.S3_ENDPOINT;
 const region = process.env.REGION;
 const bucket = process.env.DATA_BUCKET;
 const dynamo = new AWS.DynamoDB.DocumentClient({endpoint: dynamoEndpoint, apiVersion: '2012-08-10'});
-const s3 = new AWS.S3({region: region, apiVersion: '2006-03-01', s3ForcePathStyle: true});
+const s3 = new AWS.S3({endpoint: s3Endpoint, apiVersion: '2006-03-01', s3ForcePathStyle: true});
 
 const sqlite3 = require('better-sqlite3');
 const parse = require('csv-parse');