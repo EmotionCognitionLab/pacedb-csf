4dfd58f76a67ea8a408439838adaf19205f3a89c
Noah Mercer <noah_ten@yahoo.com>
2018-02-19 17:04:52 -0800

    Adds ability to optionally push new git version to remote repo. Handles case where no git version has been set yet. Fixes formatting, error outupt bugs.

--- client/scripts/version-deploy.js
+++ client/scripts/version-deploy.js
@@ -14,6 +14,7 @@
  * And yes, we intentionally allow the npm package version and the git version to differ. The git version will be
  * incremented when we deploy the client *or* the server; the package version for the client will only be 
  * incrememted when we deploy the client.)
+ *  6. Optionally pushes new version to remote repo.
 */
 
 const { spawnSync } = require('child_process');
@@ -25,6 +26,7 @@ prompt.delimiter = '';
 
 function getCurGitVersion() {
     const git = spawnSync('git', ['tag', '-l', '--sort=v:refname', '[0-9]*']);
+    if (git.stdout.toString() === '') return '0.0.0';
     return git.stdout.toString().split('\n').filter(f => f !== '').pop();
 }
 
@@ -77,6 +79,27 @@ function requestVersion(curVersion, suggestedVersion) {
     });       
 }
 
+function requestYesNo(msg) {
+    const schema = {
+        properties: {
+            continue: {
+                pattern: /[yYnN]/,
+                message: 'Please answer Y (yes) or N (no):',
+                description: msg,
+                required: true
+            }
+        }
+    };
+    return new Promise((resolve, reject) => {
+        prompt.get(schema, function(err, result) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(result.continue);
+            }
+        });
+    });
+}
 
 const curPkgVersion = process.argv[2];
 console.log(`Current package version is ${curPkgVersion}`);
@@ -85,8 +108,8 @@ requestVersion(curPkgVersion, patchedPkgVersion)
 .then(nextPkgVersion => {
     const npmBump = spawnSync('npm',['version', nextPkgVersion]);
     if (npmBump.status !== 0 || npmBump.error) {
-        const npmErr = `npm version exited with status ${npmBump.status}`
-        console.log(npmBump.stderr);
+        const npmErr = `npm version exited with status ${npmBump.status}`;
+        console.log(npmBump.stderr.toString());
         if (npmBump.error) {
             throw npmBump.error;
         } else {
@@ -96,8 +119,8 @@ requestVersion(curPkgVersion, patchedPkgVersion)
 
     const gitAdd = spawnSync('git', ['add', 'package.json', 'package-lock.json']);
     if (gitAdd.status !== 0 || gitAdd.error) {
-        const gitAddErr = `git add exited with status ${gitAdd.status}`
-        console.log(gitAdd.stderr);
+        const gitAddErr = `git add exited with status ${gitAdd.status}`;
+        console.log(gitAdd.stderr.toString());
         if (gitAdd.error) {
             throw gitAdd.error;
         } else {
@@ -107,8 +130,8 @@ requestVersion(curPkgVersion, patchedPkgVersion)
 
     const gitCommit = spawnSync('git', ['commit', '-m', 'bumping package version']);
     if (gitCommit.status !== 0 || gitCommit.error) {
-        const gitCommitErr = `git commit exited with status ${gitCommit.status}`
-        console.log(gitCommit.stderr);
+        const gitCommitErr = `git commit exited with status ${gitCommit.status}`;
+        console.log(gitCommit.stderr.toString());
         if (gitCommit.error) {
             throw gitCommit.error;
         } else {
@@ -125,8 +148,8 @@ requestVersion(curPkgVersion, patchedPkgVersion)
 .then(newGitVersion => {
     const gitTag = spawnSync('git', ['tag', '-a', newGitVersion, '-m', `Bumping to version ${newGitVersion}`]);
     if (gitTag.status !== 0 || gitTag.error) {
-        const gitTagErr = `git add exited with status ${gitTag.status}`
-        console.log(gitTag.stderr);
+        const gitTagErr = `git tag exited with status ${gitTag.status}`;
+        console.log(gitTag.stderr.toString());
         if (gitTag.error) {
             throw gitTag.error;
         } else {
@@ -134,7 +157,27 @@ requestVersion(curPkgVersion, patchedPkgVersion)
         }
     }
 })
-.catch(err => console.log(err))
+.then(() => {
+    return requestYesNo('Push new version tag to remote repository? (Y/N):');
+})
+.then(answer => {
+    if (answer.toUpperCase() === 'Y') {
+        const gitPush = spawnSync('git', ['push', '--tags'], {stdio: 'inherit'}); // TODO decide what to do about branches
+        if (gitPush.status !== 0 || gitPush.error) {
+            const gitPushErr = `git push exited with status ${gitPush.status}`;
+            console.log(gitPush.stderr);
+            if (gitPush.error) {
+                throw gitPush.error;
+            } else {
+                throw new Error(gitPushErr);
+            }
+        }
+    }
+})
+.catch(err => {
+    console.log(err);
+    process.exit(1);
+})
 
 
 
