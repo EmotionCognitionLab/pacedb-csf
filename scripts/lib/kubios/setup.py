ca7e81acdabb479a7514b5ad2eeba6d9ac28cf81
Noah Mercer <noah_ten@yahoo.com>
2019-02-08 15:49:55 -0800

    Specifies deps for the kubios lib

--- scripts/lib/kubios/setup.py
+++ scripts/lib/kubios/setup.py
@@ -1,5 +1,10 @@
 from setuptools import setup, find_packages
 setup(
+    install_requires=[
+    'colorama',
+    'datetime',
+    'pywinauto'
+    ],
     name="kubios",
     version="0.1",
     packages=find_packages(),
