9a4f90eddb475d858be7d72c9e508d7bd81b670d
Noah Mercer <noah_ten@yahoo.com>
2019-02-08 15:51:16 -0800

    First cut at kubios automation with local input files

---
+++ scripts/kubios-automation/emwave.py
@@ -0,0 +1,49 @@
+import sqlite3
+import time
+
+class EmwaveDb:
+
+    def __init__(self, db_file_path):
+        self.path = db_file_path
+        self.conn = None
+        self.c = None
+
+    def open(self):
+        self.conn = sqlite3.connect(self.path)
+        self.c = self.conn.cursor()
+
+    def close(self):
+        if self.conn:
+            self.conn.close()
+
+    def _confirm_db_open(self):
+        if not self.conn:
+            raise Exception('You must call open() before fetching sessions')
+
+    def fetch_session_rr_data(self, username, start_date, end_date):
+        """Returns list of sessions. Each session contains a list of RR intervals (ms between heartbeats).
+
+        username   - The name of the user (as found in User.FirstName in the emwave database) whose data you want
+        start_date - time_struct (referring to local time). Sessions starting earlier than this will be ignored.
+        end_date   - time_struct (referring to local time). Sessions starting later than this will be ignored.
+        """
+        
+        self._confirm_db_open()
+
+        sessions = list()
+        stmt = 'select LiveIBI from Session s join User u on s.UserUuid = u.UserUuid where u.FirstName = ? and s.ValidStatus = 1 and s.DeleteFlag is null and s.IBIStartTime >= ? and s.IBIEndTime <= ? order by IBIStartTime asc'
+        for row in self.c.execute(stmt, (username, time.mktime(start_date), time.mktime(end_date))):
+            rr_data = list()
+            for i in range(0, len(row[0]), 2):
+                rr_data.append(int.from_bytes(row[0][i:i+2], byteorder='little'))
+            sessions.append(rr_data)
+
+        return sessions
+
+    def fetch_user_first_names(self):
+        self._confirm_db_open()
+        self.c.execute('select FirstName from User')
+        return [i[0] for i in self.c.fetchall()]
+
+
+    
---
+++ scripts/kubios-automation/main.py
@@ -0,0 +1,112 @@
+import emwave as em
+import kubios as ka
+from pathlib import Path, PurePath
+import sys
+import tempfile
+
+EMWAVE_FILE_TYPE = 'em'
+ACQ_FILE_TYPE = 'acq'
+PULSE_TEXT_FILE_TYPE = 'txt'
+
+# Output is stored in a dir that's a sibling to the input dir
+# Directory will be created if it doesn't already exist
+OUTPUT_DIR_NAME = 'output'
+
+FILE_TYPE_TO_EXTENSION = {
+    EMWAVE_FILE_TYPE: '.emdb', 
+    ACQ_FILE_TYPE: '.acq',
+    PULSE_TEXT_FILE_TYPE: '.txt'
+}
+
+def get_run_info():
+    file_type = input("File type (emWave [{}], Pulse ACQ [a{}], Pulse Text [{}]): ".format(EMWAVE_FILE_TYPE, ACQ_FILE_TYPE, PULSE_TEXT_FILE_TYPE))
+    input_dir = input("Directory with input files: ")
+    while file_type != EMWAVE_FILE_TYPE:
+        file_type = input("Currently only emWave files are supported. File type: ")
+
+    return (file_type, input_dir)
+
+def get_input_files(input_dir, file_type):
+    file_ext = FILE_TYPE_TO_EXTENSION[file_type]
+    input_path = Path(input_dir)
+    return [f for f in input_path.iterdir() if f.is_file() and PurePath(f).suffix == file_ext]
+
+def write_emwave_data_to_file(fname, user_name):
+    emwave_db = em.EmwaveDb(fname)
+    emwave_db.open()
+    sessions = emwave_db.fetch_session_rr_data(user_name, (2000, 0, 0, 0, 0, 0, 0, 0, -1), (3000, 0, 0, 0, 0, 0, 0, 0, -1))
+    emwave_db.close()
+
+    rr_file_names = list()
+    for idx, rr_data in enumerate(sessions):
+        fname = tempfile.NamedTemporaryFile(suffix='.txt', prefix='{}-session{:02d}.'.format(user_name, idx), delete=False)
+        rr_file_names.append(fname.name)
+        with open(fname.name, 'w') as f:
+            for d in rr_data:
+                f.write("%d\n" % d)
+    
+    return rr_file_names
+
+def make_output_dir_if_not_exists(input_dir):
+    input_path = Path(input_dir)
+    output_path = input_path.parent / OUTPUT_DIR_NAME
+    if not output_path.is_dir():
+        if output_path.is_file():
+            raise Exception('{} should be a directory, but is a file.'.format(output_path))
+        
+        output_path.mkdir()
+    return output_path
+
+def get_valid_response(msg, valid_responses):
+    """Requests user input, checks that input is in list of valid_responses and returns response.
+    Loops until user returns a valid response."""
+    is_valid = False
+    while not is_valid:
+        resp = input(msg)
+        if valid_responses.count(resp) == 0:
+            print("{} is not a valid response.".format(resp))
+            continue
+        
+        return resp
+
+if __name__ == "__main__":
+    (file_type, input_dir) = get_run_info()
+    output_path = make_output_dir_if_not_exists(input_dir)
+    input_files = get_input_files(input_dir, file_type)
+    if file_type == EMWAVE_FILE_TYPE:
+        rr_files = list()
+        for emdb in input_files:
+            db = em.EmwaveDb(emdb)
+            db.open()
+            emwave_user_names = db.fetch_user_first_names()
+            db.close()
+            for name in emwave_user_names:
+                should_process = get_valid_response("Process user {} from {}? [Y(es)/n(o)/s(kip) to next emWave file]".format(name, emdb), ['', 'Y', 'y', 'N', 'n', 'S', 's'])
+                if should_process == '' or should_process == 'Y' or should_process == 'y':
+                    rr_files.extend(write_emwave_data_to_file(str(emdb), name))
+                elif should_process == 'N' or should_process == 'n':
+                    continue
+                elif should_process == 'S' or should_process == 's':
+                    break
+        
+        # now that we've sucked the RR data from the emwave file we just proceed with a bunch of RR data files
+        input_files = rr_files
+    else:
+        print('Only emwave files are currently supported.')
+        sys.exit()
+
+    for f in input_files:
+        if file_type == EMWAVE_FILE_TYPE:
+            app = ka.kubios_get_app()
+            ka.kubios_open_rr_file(app, f)
+            kubios_window = app.window(title_re='Kubios.*$', class_name='SunAwtFrame')
+            ka.kubios_analyse(kubios_window)
+
+            name_no_ext = PurePath(f).stem
+            results_path = output_path / name_no_ext
+            ka.kubios_save_results(app, str(results_path), f)
+            ka.kubios_close_file(kubios_window)
+        else:
+            print('Only emwave files are currently supported.')
+            sys.exit()
+        
