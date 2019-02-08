5b3eb40dde84a6505f5c2463b8be10e2f1370dda
Noah Mercer <noah_ten@yahoo.com>
2019-02-08 13:59:47 -0800

    Moves kubios functionality to separate module

--- scripts/calibration/calibration.py
+++ scripts/calibration/calibration.py
@@ -1,9 +1,9 @@
 
+import kubios
 import boto3
 import botocore
 from colorama import init
 init(autoreset=True)
-from datetime import datetime
 import gspread
 import h5py
 import json
@@ -11,14 +11,13 @@ import moment
 from oauth2client.service_account import ServiceAccountCredentials
 import os
 from pathlib import Path
-from pywinauto.application import Application
-from pywinauto.findwindows import ElementNotFoundError, find_windows
-from pywinauto.win32functions import SetForegroundWindow
 import requests
 import sys
 import tempfile
-import time
 import traceback
+
+# Path to kubios application
+KUBIOS_PATH = 'C:/Program Files/Kubios/Kubios HRV Premium/kubioshrv.exe'
 
 # ID of the Google spreadsheet we store the data in
 SHEET_ID = '1Qs2MNm0IkjQguA6N6zQagh1sToay-nb7G0g926Du96M'
@@ -32,11 +31,6 @@ AWS_KEY_FILE = './aws-key.json'
 # info for lambda access
 # Example file: { "key": "<your api key>", "url": "https://<your aws api gateway host>.execute-api.<aws region>.amazonaws.com/dev/subjects/{0}/calibration" }
 API_CONFIG_FILE = './api-config.json'
-
-KUBIOS_PATH = 'C:/Program Files/Kubios/Kubios HRV Premium/kubioshrv.exe'
-
-# Expected value for the AR model preference setting in Kubios
-KUBIOS_AR_MODEL = 16
 
 # Top-level bucket where calibration data should be stored
 DATA_BUCKET = 'hrv-usr-data'
@@ -204,118 +198,6 @@ def get_run_info():
 
     return (subject_id, week, date_cutoff)    
 
-def kubios_get_app():
-    try:
-        app=Application().connect(title_re='Kubios.*$', class_name='SunAwtFrame')
-        warn('Kubios is already running.')
-        print('Please make sure that any open analyses are saved and closed before continuing.')
-        response = ''
-        while response != 'c' and response != 'q':
-            response = input("Press 'c' to continue or 'q' to quit:")
-            if response == 'c':
-                return app
-            if response == 'q': 
-                sys.exit(0)
-    except ElementNotFoundError:
-        app=Application().start(KUBIOS_PATH)
-    
-    return app
-
-def kubios_open_rr_file(kubios_app, rr_file_path):
-    kubios_window = kubios_app.window(title_re='Kubios.*$', class_name='SunAwtFrame')
-    kubios_window.wait('visible', 120)
-    kubios_window.type_keys('^O') # Ctrl-O
-    open_dlg = kubios_app.window(title='Get Data File')
-    open_dlg.type_keys('foo') # hack to get the file name text entry box to have focus; the 'foo' isn't actually captured by it
-    open_dlg.get_focus().type_keys(rr_file_path + '{ENTER}', with_spaces=True)
-    while kubios_is_processing(kubios_app):
-        pass
-        # do nothing; just wait for it to finish opening the file
-
-def kubios_save_results(kubios_app, results_file_path, input_fname):
-    kubios_window = kubios_app.window(title_re='Kubios.*$', class_name='SunAwtFrame')
-    kubios_window.type_keys('^S') # Ctrl-S
-    save_dlg = kubios_app.window(title='Save as')
-    save_dlg.wait('ready')
-
-    # Set the 'Save as' type
-    combo_boxes = save_dlg.children(title='Save all (*.txt,*.mat,*.pdf)')
-    if len(combo_boxes) != 1:
-        warn('Could not find "Save as type:" pull-down menu while saving - using default.')
-    else:
-        save_as_combo_box = combo_boxes[0]
-        save_as_combo_box.select(0)
-    
-    # Set the filename
-    combo_boxes = save_dlg.children(title=kubios_default_save_as_fname(input_fname), class_name='Edit')
-    if len(combo_boxes) != 1:
-        raise Exception('Could not find text field for file name in save dialog.')
-    
-    combo_boxes[0].type_keys(results_file_path + '{ENTER}', with_spaces=True)
-    
-    # TODO find better way to accomodate the delay between submitting save dlg
-    # and appearance of "processing" dialogs associated with saving
-    # maybe just check for existence of output files?
-    time.sleep(7)
-    while kubios_is_processing(kubios_app):
-        pass
-        # do nothing; just wait for it to finish saving the results
-
-def kubios_default_save_as_fname(input_fname):
-    """As a default file name for the results kubios suggests the input file name with the extension replaced with '_hrv'."""
-    return input_fname.split('.')[0] + '_hrv'
-
-def kubios_close_file(kubios_window):
-    kubios_window.type_keys('^W') # Ctrl-W
-
-def kubios_analyse(kubios_window, delay=2):
-    """Applies artifact correction and sets the start and length of the sample.
-    The elements in the Kubios UI can be given focus by tabbing through them.
-    They're organized (by kubios) in a particular order, and pressing tab will
-    take you through them in that order (while shift+tab takes you backward).
-    For that reason it's very important that the order of the operations here
-    not be changed without careful testing.
-    """
-    kubios_window.type_keys('{TAB}')   # give focus to artifact correction menu
-    kubios_window.type_keys('{DOWN}')  # use down arrow to select 1st item in artifact correction menu
-    time.sleep(delay)
-    kubios_window.type_keys('+{TAB}')  # use shift-tab to select the 'Apply' button
-    kubios_window.type_keys('{VK_SPACE}') # to press the 'Apply' button
-    time.sleep(delay)
-    kubios_window.type_keys('{TAB 5}') # 5 tabs to select the sample length text field
-    kubios_window.type_keys('00:04:00') # set the length
-    time.sleep(delay)
-    kubios_window.type_keys('+{TAB}')  # shift-tab to select the sample start text field
-    kubios_window.type_keys('00:00:30') # set the start
-    kubios_window.type_keys('{TAB}')   # to get kubios to recognize the change we made to the start field
-    time.sleep(delay)
-
-def kubios_is_processing(kubios_app):
-    """ When opening or saving a file Kubios can throw up multiple 'Processing...' dialogs.
-    This will find one, wait until it doesn't exist, and repeat until no such
-    dialog has existed for 4 seconds.
-    """
-    test_start = datetime.now()
-    test_end = datetime.now()
-    while (test_end - test_start).seconds < 4:
-        proc_dlg_count = len(kubios_app.windows(title='Processing...'))
-        if proc_dlg_count == 0:
-                time.sleep(1)
-                test_end = datetime.now()
-        else:
-            test_start = datetime.now()
-            test_end = datetime.now()
-
-    return False
-
-def expand_windows_short_name(short_name):
-    from ctypes import create_unicode_buffer, windll
-    buf_size = 500
-    buffer = create_unicode_buffer(buf_size)
-    get_long_path_name = windll.kernel32.GetLongPathNameW
-    get_long_path_name(short_name, buffer, buf_size)
-    return buffer.value
-
 def expected_output_files(fname_prefix):
     """Given a filename prefix (which may or may not contain a full directory path)
     entered into the Kubios save dialog, return a list of
@@ -388,7 +270,7 @@ if __name__ == "__main__":
             print('No data found for subject id {0} after {1}.'.format(subject_id, cutoff_date))
             wait_and_exit(0)
 
-        temp_dir = expand_windows_short_name(tempfile.gettempdir())
+        temp_dir = kubios.expand_windows_short_name(tempfile.gettempdir())
         for i in range(0, session_count):
             print("Processing session {0} of {1}...".format(i+1, session_count))
 
@@ -398,15 +280,27 @@ if __name__ == "__main__":
             print("RR data saved to file", rr_data_file)
 
             print("Running Kubios analysis...")
-            app = kubios_get_app()
-            kubios_open_rr_file(app, rr_data_file)
+            try:
+                app = kubios.get_app(KUBIOS_PATH)
+            except kubios.KubiosRunningError:
+                warn('Kubios is already running.')
+                print('Please make sure that any open analyses are saved and closed before continuing.')
+                response = ''
+                while response != 'c' and response != 'q':
+                    response = input("Press 'c' to continue or 'q' to quit:")
+                    if response == 'c':
+                        app = kubios.get_app(KUBIOS_PATH, True)
+                    if response == 'q':
+                        sys.exit(0)
+
+            kubios.open_rr_file(app, rr_data_file)
             win = app.window(title_re='Kubios.*$', class_name='SunAwtFrame')
-            kubios_analyse(win)
+            kubios.analyse(win)
             results_path = '{0}\\{1}_week{2}_{3}'.format(temp_dir, subject_id, week, str(i + 1))
 
             print("Saving Kubios results to {}...".format(results_path))
-            kubios_save_results(app, results_path, rr_fname)
-            kubios_close_file(win)
+            kubios.save_results(app, results_path, rr_fname)
+            kubios.close_file(win)
             if not expected_kubios_output_files_ok(results_path):
                 wait_and_exit(1)
             kubios_data_file = results_path + '.mat'
---
+++ scripts/lib/kubios/kubios.py
@@ -0,0 +1,120 @@
+from colorama import init
+init(autoreset=True)
+from datetime import datetime
+from pywinauto.application import Application
+from pywinauto.findwindows import ElementNotFoundError, find_windows
+from pywinauto.win32functions import SetForegroundWindow
+import time
+
+class KubiosRunningError(Exception):
+    """Marker error raised when we try to start Kubios and find it's already running"""
+    pass
+
+def get_app(path_to_app, already_running_ok=False):
+    try:
+        app=Application().connect(title_re='Kubios.*$', class_name='SunAwtFrame')
+        if not already_running_ok: raise KubiosRunningError
+        
+    except ElementNotFoundError:
+        app=Application().start(path_to_app)
+    
+    return app
+
+def open_rr_file(kubios_app, rr_file_path):
+    kubios_window = kubios_app.window(title_re='Kubios.*$', class_name='SunAwtFrame')
+    kubios_window.wait('visible', 120)
+    kubios_window.type_keys('^O') # Ctrl-O
+    open_dlg = kubios_app.window(title='Get Data File')
+    open_dlg.type_keys('foo') # hack to get the file name text entry box to have focus; the 'foo' isn't actually captured by it
+    open_dlg.get_focus().type_keys(rr_file_path + '{ENTER}', with_spaces=True)
+    while is_processing(kubios_app):
+        pass
+        # do nothing; just wait for it to finish opening the file
+
+def save_results(kubios_app, results_file_path, input_fname):
+    kubios_window = kubios_app.window(title_re='Kubios.*$', class_name='SunAwtFrame')
+    kubios_window.type_keys('^S') # Ctrl-S
+    save_dlg = kubios_app.window(title='Save as')
+    save_dlg.wait('ready')
+
+    # Set the 'Save as' type
+    combo_boxes = save_dlg.children(title='Save all (*.txt,*.mat,*.pdf)')
+    if len(combo_boxes) != 1:
+        warn('Could not find "Save as type:" pull-down menu while saving - using default.')
+    else:
+        save_as_combo_box = combo_boxes[0]
+        save_as_combo_box.select(0)
+    
+    # Set the filename
+    combo_boxes = save_dlg.children(title=default_save_as_fname(input_fname), class_name='Edit')
+    if len(combo_boxes) != 1:
+        raise Exception('Could not find text field for file name in save dialog.')
+    
+    combo_boxes[0].type_keys(results_file_path + '{ENTER}', with_spaces=True)
+    
+    # TODO find better way to accomodate the delay between submitting save dlg
+    # and appearance of "processing" dialogs associated with saving
+    # maybe just check for existence of output files?
+    time.sleep(7)
+    while is_processing(kubios_app):
+        pass
+        # do nothing; just wait for it to finish saving the results
+
+def default_save_as_fname(input_fname):
+    """As a default file name for the results kubios suggests the input file name with the extension replaced with '_hrv'."""
+    return input_fname.split('.')[0] + '_hrv'
+
+def close_file(kubios_window):
+    kubios_window.type_keys('^W') # Ctrl-W
+
+def analyse(kubios_window, delay=2):
+    """Applies artifact correction and sets the start and length of the sample.
+    The elements in the Kubios UI can be given focus by tabbing through them.
+    They're organized (by kubios) in a particular order, and pressing tab will
+    take you through them in that order (while shift+tab takes you backward).
+    For that reason it's very important that the order of the operations here
+    not be changed without careful testing.
+    """
+    kubios_window.type_keys('{TAB}')   # give focus to artifact correction menu
+    kubios_window.type_keys('{DOWN}')  # use down arrow to select 1st item in artifact correction menu
+    time.sleep(delay)
+    kubios_window.type_keys('+{TAB}')  # use shift-tab to select the 'Apply' button
+    kubios_window.type_keys('{VK_SPACE}') # to press the 'Apply' button
+    time.sleep(delay)
+    kubios_window.type_keys('{TAB 5}') # 5 tabs to select the sample length text field
+    kubios_window.type_keys('00:04:00') # set the length
+    time.sleep(delay)
+    kubios_window.type_keys('+{TAB}')  # shift-tab to select the sample start text field
+    kubios_window.type_keys('00:00:30') # set the start
+    kubios_window.type_keys('{TAB}')   # to get kubios to recognize the change we made to the start field
+    time.sleep(delay)
+
+def is_processing(kubios_app):
+    """ When opening or saving a file Kubios can throw up multiple 'Processing...' dialogs.
+    This will find one, wait until it doesn't exist, and repeat until no such
+    dialog has existed for 4 seconds.
+    """
+    test_start = datetime.now()
+    test_end = datetime.now()
+    while (test_end - test_start).seconds < 4:
+        proc_dlg_count = len(kubios_app.windows(title='Processing...'))
+        if proc_dlg_count == 0:
+                time.sleep(1)
+                test_end = datetime.now()
+        else:
+            test_start = datetime.now()
+            test_end = datetime.now()
+
+    return False
+
+def expand_windows_short_name(short_name):
+    from ctypes import create_unicode_buffer, windll
+    buf_size = 500
+    buffer = create_unicode_buffer(buf_size)
+    get_long_path_name = windll.kernel32.GetLongPathNameW
+    get_long_path_name(short_name, buffer, buf_size)
+    return buffer.value
+
+def warn(msg):
+    """Prints warning message in yellow text"""
+    print("\033[93m WARNING: {}\033[00m".format(msg))
