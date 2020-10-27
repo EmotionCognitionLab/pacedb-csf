ed1b6f5a2357242966dab91dc50cefbb104669ee
Noah Mercer <noah_ten@yahoo.com>
2020-10-27 12:17:15 -0700

    Removes unnecessary code for qualtrics survey

--- server/qualtrics/test/env.sh
+++
@@ -1,7 +0,0 @@
-DYNAMO_ENDPOINT=http://localhost:8000
-QUALTRICS_HOST=http://localhost:7001
-QUALTRICS_API_KEY=some-api-key
-USERS_TABLE=hrv-users
-AWS_ACCESS_KEY_ID=accessKey
-AWS_SECRET_ACCESS_KEY=secret
-REGION=us-west-2
